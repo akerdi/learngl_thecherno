92cd3a599ccafe6a5465476a3d01e8fe1f0dcfb5
少鸿 江 <tianxiaoxin001@gmail.com>
2021-08-18 13:33:27 +0800

    init

---
+++ main.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+using namespace std;
+int main(void)
+{
+    GLFWwindow* window;
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+    cout << GL_VERSION << endl;
+    glfwTerminate();
+    return 0;
+}
