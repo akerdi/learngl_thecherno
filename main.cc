64d59b25f4935d2720f150cd18148b539237aaba
少鸿 江 <tianxiaoxin001@gmail.com>
2021-08-20 13:40:17 +0800

    fix glew库导入失败

--- main.cc
+++ main.cc
@@ -1,33 +1,72 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-using namespace std;
-int main(void)
-{
+
+
+// 回调函数声明
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 640;
+const unsigned int SCR_HEIGHT = 480;
+
+int main(int argc, char * argv[]) {
+    // 初始化GLFW
     GLFWwindow* window;
-    /* Initialize the library */
-    if (!glfwInit())
+    if (!glfwInit()) {
         return -1;
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    }
+    // 创建一个窗口对象
+    window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "opengl", NULL, NULL);
     if (!window)
     {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-        /* Poll for and process events */
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error ! " << std::endl;
+    std:: cout << glGetString(GL_VERSION) << std::endl;
+
+    float positions[6] = {
+        -0.5f, -0.5f,
+        0.0f, 0.5f,
+        0.5f, -0.5f
+    };
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer); // select the buffer
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+
+    while(!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window); // 交换颜色缓冲
+
         glfwPollEvents();
     }
-    cout << GL_VERSION << endl;
+
+    // 释放/删除之前的分配的所有资源
     glfwTerminate();
-    return 0;
+    return EXIT_SUCCESS;
+}
+
+// 输入控制，检查用户是否按下了返回键(Esc)
+void processInput(GLFWwindow *window) {
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// 当用户改变窗口的大小的时候，视口也应该被调整
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+    // 注意：对于视网膜(Retina)显示屏，width和height都会明显比原输入值更高一点。
+    glViewport(0, 0, width, height);
 }
